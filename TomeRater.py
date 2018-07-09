2b1e31474152f59a5111e92971cd75a41f45a6da
Valhalla <martinfloorball1997@gmail.com>
2018-07-09 10:34:38 +0200

    Added the files

---
+++ TomeRater.py
@@ -0,0 +1,20 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        # Empty dictionary that is going to hold the user's rating of the book
+        self.books = {}
+
+    # Method that returns an email on the user
+    def get_email(self):
+        return self.email
+
+    # Method to change the email on the selected user
+    def change_email(self, address):
+
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other_user):
+        pass
---
+++ populate.py
@@ -0,0 +1,42 @@
+from TomeRater import *
+
+Tome_Rater = User()
+
+#Create some books:
+book1 = Tome_Rater.create_book("Society of Mind", 12345678)
+novel1 = Tome_Rater.create_novel("Alice In Wonderland", "Lewis Carroll", 12345)
+novel1.set_isbn(9781536831139)
+nonfiction1 = Tome_Rater.create_non_fiction("Automate the Boring Stuff", "Python", "beginner", 1929452)
+nonfiction2 = Tome_Rater.create_non_fiction("Computing Machinery and Intelligence", "AI", "advanced", 11111938)
+novel2 = Tome_Rater.create_novel("The Diamond Age", "Neal Stephenson", 10101010)
+novel3 = Tome_Rater.create_novel("There Will Come Soft Rains", "Ray Bradbury", 10001000)
+
+#Create users:
+Tome_Rater.add_user("Alan Turing", "alan@turing.com")
+Tome_Rater.add_user("David Marr", "david@computation.org")
+
+#Add a user with three books already read:
+Tome_Rater.add_user("Marvin Minsky", "marvin@mit.edu", user_books=[book1, novel1, nonfiction1])
+
+#Add books to a user one by one, with ratings:
+Tome_Rater.add_book_to_user(book1, "alan@turing.com", 1)
+Tome_Rater.add_book_to_user(novel1, "alan@turing.com", 3)
+Tome_Rater.add_book_to_user(nonfiction1, "alan@turing.com", 3)
+Tome_Rater.add_book_to_user(nonfiction2, "alan@turing.com", 4)
+Tome_Rater.add_book_to_user(novel3, "alan@turing.com", 1)
+
+Tome_Rater.add_book_to_user(novel2, "marvin@mit.edu", 2)
+Tome_Rater.add_book_to_user(novel3, "marvin@mit.edu", 2)
+Tome_Rater.add_book_to_user(novel3, "david@computation.org", 4)
+
+
+#Uncomment these to test your functions:
+# Tome_Rater.print_catalog()
+# Tome_Rater.print_users()
+
+# print("Most positive user:")
+# print(Tome_Rater.most_positive_user())
+# print("Highest rated book:")
+# print(Tome_Rater.highest_rated_book())
+# print("Most read book:")
+# print(Tome_Rater.get_most_read_book())
